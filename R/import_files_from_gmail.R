4ae5f1286c2658b5fc87230083a3442e4b3550da
Vincent Guyader <vincent@thinkr.fr>
2019-09-29 08:50:29 +0200

    use safe_save_attachments

--- R/import_files_from_gmail.R
+++ R/import_files_from_gmail.R
@@ -5,13 +5,15 @@
 #' @return
 #' @export
 #' @importFrom gmailr message save_attachments
-#' @importFrom purrr compact map
+#' @importFrom purrr compact map possibly
 #' @importFrom magrittr %>%
 import_files_from_gmail <- function(messages_id,path="."){
+
+  safe_save_attachments <- possibly(save_attachments,otherwise="nope")
   messages_id %>%
   map(message) %>%
-  map(save_attachments,path=path) %>%
-  compact() %>%
+  map(safe_save_attachments,path=path) %>%
+  # compact() %>%
   unlist() %>%
   map_chr(clean_rename)
 }
